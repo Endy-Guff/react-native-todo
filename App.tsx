6bdda9447cb19120f1497935e1d50a3655dbcb0d
Endy-Guff <andreymaranchak@yandex.ru>
2023-11-06 13:50:40 +0300

    Init commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, TextInput, View} from 'react-native';
+import {useState} from "react";
+import {v4} from 'uuid';
+
+interface TodoType {
+    id: string,
+    title: string,
+    isDone: boolean
+}
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [inputValue, setInputValue] = useState('')
+
+    const [todo, setTodo] = useState<TodoType[]>([
+        {id: v4(), title: 'qew', isDone: false},
+        {id: v4(), title: 'zxc', isDone: true},
+    ])
+
+    return (
+        <View style={styles.container}>
+            <TextInput style={styles.input} value={inputValue} onChangeText={setInputValue}/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        paddingVertical: 80,
+        backgroundColor: '#ffffff',
+        display: "flex",
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    input: {
+        width: 300,
+        height: 50,
+        backgroundColor: '#e0e0e0',
+        padding: 6
+    }
 });
